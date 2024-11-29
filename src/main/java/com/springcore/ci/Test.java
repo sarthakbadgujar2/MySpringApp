80e846ac888fab34b91d0f4f6f8a02131b9f2a19
sarthakb <sarthakb@winjit.com>
2024-11-29 14:35:02 +0530

    constructor injection

---
+++ src/main/java/com/springcore/ci/Test.java
@@ -0,0 +1,18 @@
+package com.springcore.ci;
+
+import org.example.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("ciConfig.xml");
+        Person person = (Person) context.getBean("person");
+        System.out.println(person);
+
+        // Add any additional logic for using the context (e.g., retrieving beans)
+
+
+    }
+}
